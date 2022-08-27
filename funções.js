81858f6ea9ad5a5fbaf4d9b9f4556988f064354b
DanyhMA <108550443+DanyhMA@users.noreply.github.com>
2022-08-27 16:54:14 -0300

    Add files via upload

---
+++ funções.js
@@ -0,0 +1,13 @@
+function dia(){
+    alert('BOM DIA!')
+}
+
+function soma (n1, n2){
+    resultado=n1+n2
+    alert ('O resultado da soma é: ' + resultado)
+}
+
+saudacao=(nome)=> 'Bom dia, ${nome}'
+
+dia()
+soma(5, 6)
